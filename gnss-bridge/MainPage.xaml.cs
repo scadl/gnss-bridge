7c682c852d20c958a232e021f3a21980f87f4359
Alexnadr <scad.luncher@gmail.com>
2020-03-14 20:36:38 +0300

    Fully working GNSS2COM and NMEA convert

--- gnss-bridge/MainPage.xaml.cs
+++ gnss-bridge/MainPage.xaml.cs
@@ -13,6 +13,7 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
+using System.IO.Ports;
 using Windows.Devices.Geolocation;
 using Windows.UI.Core;
 
@@ -26,11 +27,18 @@ namespace gnss_bridge
 
         Geolocator myGeolocator;
         int iterator = 0;
+        uint scanInterval = 3000;
+        SerialPort outCOM;
 
         public MainPage()
         {
             this.InitializeComponent();
-            getAccStatus();
+        }
+
+        private async void GetCOM()
+        {            
+            outCOM = new SerialPort(tbPort.Text);
+            outCOM.Open();
         }
 
         private async void getAccStatus()
@@ -40,7 +48,7 @@ namespace gnss_bridge
             switch (accesStatus)
             {
                 case GeolocationAccessStatus.Allowed:
-                    myGeolocator = new Geolocator() { ReportInterval = 3000 };
+                    myGeolocator = new Geolocator() { ReportInterval = scanInterval };
                     myGeolocator.StatusChanged += getGeo_StatusChange;
                     myGeolocator.PositionChanged += getGeo_PositionChange;
                     txtLineState.Text = "Waiting for update...";
@@ -55,27 +63,76 @@ namespace gnss_bridge
             }         
         }
 
+        private static string getChecksum(string sentence)
+        {
+            //Start with first Item
+            int checksum = Convert.ToByte(sentence[sentence.IndexOf('$') + 1]);
+            // Loop through all chars to get a checksum
+            for (int i = sentence.IndexOf('$') + 2; i < sentence.IndexOf('*'); i++)
+            {
+                // No. XOR the checksum with this character's value
+                checksum ^= Convert.ToByte(sentence[i]);
+            }
+            // Return the checksum formatted as a two-character hexadecimal
+            return checksum.ToString("X2");
+        }
+
         private async void getGeo_PositionChange(Geolocator sender, PositionChangedEventArgs evtPostition)
         {
+            String stateData = "";
+
             await Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
             {
                 txtLineState.Text = "Location updated.";
                 iterator += 1;                
 
-                txtLineRes1.Text = evtPostition.Position.Coordinate.Latitude.ToString();
-                txtLineRes2.Text = evtPostition.Position.Coordinate.Longitude.ToString();
-                txtLineRes3.Text = evtPostition.Position.Coordinate.Accuracy.ToString();
-                txtLineRes4.Text = iterator.ToString();                
+                txtLineRes1.Text = evtPostition.Position.Coordinate.Point.Position.Latitude.ToString() + " degrees.";
+                txtLineRes2.Text = evtPostition.Position.Coordinate.Point.Position.Longitude.ToString() + " degrees.";
+                txtLineRes3.Text = evtPostition.Position.Coordinate.Accuracy.ToString() + " meters.";
+                txtLineRes4.Text = iterator.ToString() + " times.";
 
+                Thickness ZeroThin = new Thickness(0);
                 ListViewItem Item = new ListViewItem();
                 TextBlock myText = new TextBlock();
-                String stateData = String.Format("Latitude: {0} degrees. \nLongitude: {1} degrees. \nAccuracy: {2} meters.",
-                        evtPostition.Position.Coordinate.Latitude, evtPostition.Position.Coordinate.Longitude, evtPostition.Position.Coordinate.Accuracy);
+                //String stateData = String.Format("Latitude: {0} degrees. \nLongitude: {1} degrees. \nAccuracy: {2} meters.",
+                //        evtPostition.Position.Coordinate.Point.Position.Latitude, evtPostition.Position.Coordinate.Point.Position.Longitude, evtPostition.Position.Coordinate.Accuracy);
+                                             
+                double lat = evtPostition.Position.Coordinate.Point.Position.Latitude;
+                double lon = evtPostition.Position.Coordinate.Point.Position.Longitude;
+                                
+                string latDir = (lat >= 0 ? "N" : "S");                
+                lat = Math.Abs(lat);
+                var latMin = (lat % Math.Truncate(lat)) * 60;                
+
+                string lonDir = (lon >= 0 ? "E" : "W");                
+                lon = Math.Abs(lon);
+                var lonMin = (lon % Math.Truncate(lon)) * 60;                
+
+                var timeStamp = DateTime.UtcNow;
+                stateData = String.Format("$GPRMC,{0},A,{2}{3},{4},{5}{6},{7},,,{1},,,",
+                        timeStamp.ToString("hhmmss.sss"), timeStamp.ToString("ddMMyy"),
+                        Math.Truncate(lat).ToString().PadLeft(2,'0'), latMin.ToString().PadLeft(2,'0'), latDir,
+                        Math.Truncate(lon).ToString().PadLeft(3,'0'), lonMin.ToString().PadLeft(2,'0'), lonDir);
 
                 myText.Text = stateData;
                 Item.Content = myText;
                 cbView.Items.Add(Item);
+
+                // What's available in classes?
+                //evtPostition.Position.Coordinate.Coordinate.Point.Position.Altitude;
+                //evtPostition.Position.Coordinate.Heading;
+                //evtPostition.Position.Coordinate.Speed
             });
+
+            // Example of NMEA 0183 String:
+            // $GPRMC,125504.049,A,5542.2389,N,03741.6063,E,0.06,25.82,200906,,,*17
+            // https://ru.wikipedia.org/wiki/NMEA_0183#RMC-%D1%81%D1%82%D1%80%D0%BE%D0%BA%D0%B0_(%D1%87%D0%B0%D1%81%D1%82%D0%BD%D1%8B%D0%B9_%D0%BF%D1%80%D0%B8%D0%BC%D0%B5%D1%80)
+
+            //evtPostition.Position.Coordinate.Point.AltitudeReferenceSystem = new AltitudeReferenceSystem();
+                        
+            outCOM.WriteLine(stateData);
+
+
         }
 
         private async void getGeo_StatusChange(Geolocator sender, StatusChangedEventArgs evtStatus)
@@ -110,5 +167,31 @@ namespace gnss_bridge
           
         }
 
+        private void btnControl_Click(object sender, RoutedEventArgs e)
+        {
+            
+            if (slInterval.IsEnabled)
+            {
+                slInterval.IsEnabled = false;
+                btnControl.Content = "Stop Scan";
+                getAccStatus();
+                GetCOM();
+            } else
+            {
+                outCOM.Close();
+                slInterval.IsEnabled = true;
+                btnControl.Content = "Run Scan";
+                myGeolocator.StatusChanged -= getGeo_StatusChange;
+                myGeolocator.PositionChanged -= getGeo_PositionChange;                
+            }
+        }
+
+        private void slInterval_ValueChanged(object sender, RangeBaseValueChangedEventArgs e)
+        {
+            try { 
+                lblInterval.Text = slInterval.Value.ToString() + " sec";
+            } catch { }
+            scanInterval = Convert.ToUInt32(slInterval.Value * 1000);
+        }
     }
 }
--- gnss-bridge/Properties/AssemblyInfo.cs
+++ gnss-bridge/Properties/AssemblyInfo.cs
@@ -1,16 +1,17 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("gnss-bridge")]
+[assembly: AssemblyTitle("GNSS Data Bridge")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("gnss-bridge")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCopyright("SCAD's Design & Develop ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -27,3 +28,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: ComVisible(false)]
+[assembly: NeutralResourcesLanguage("en")]
